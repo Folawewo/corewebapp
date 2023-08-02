00784adfd19fcae2219563590243ca3bc754023a
Folawewo Anthony <famwallexie@gmail.com>
2023-08-02 23:06:37 +0100

    message view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    public IActionResult Message()
+    {
+        return View();
+    }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-﻿
+﻿##core web app
